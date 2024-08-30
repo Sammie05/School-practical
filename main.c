6dc6e2ec7583266ed3e06c4c362174ee3590e64b
Sammie05 <snweke98@gmail.com>
2024-08-30 23:34:26 +0100

    schoolpractical

---
+++ main.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own words
+	
+*/
+
+	int main(int argc, char *argv[]) {
+// decalartion instruction
+
+float x, y;
+double z;
+//assignment
+
+x = 3.5
+y = 2.6
+
+// arithmetic instruction
+z = (x + y)/2
+
+//printout
+//control instruction
+
+
+//reprocess directive
+
+#define PI 3.142 // no semicolon, no equal to, 
+//for string
+char yourName[2];
+	printf("please, enter your name: ");
+	scanf("%s", YorName);
+
+	  printf("my name is now %s", yourName);
+
+char grade;
+	printf("please, enter your grade: ");
+	scanf("%c, &grade);
+	printf("My grade is now %c in csc 202", grade);
+float cgpa;
+	
+	printf("please, enter your cgpa: ");
+	scanf("%f, &cgpa");
+
+	printf("my cgpa is now %c in csc 202", cgpa);
+
+
+	printf("Now, the value of PI is %lf");
+
+	   printf("\t Nigeria maritime university\n");
+	   printf(" warri south west \n");
+	   printf("Delta state\n");
+	   //printf("Nigeria\n");
+
+	   //format specifier
+	   printf("%d\n", 576);
+	   printf("%f\n", 5766);
+	   printf("lf\n", 767.98986);
+	   printf("%c\n", A);
+	   printf("%s\n", samuel Nweke);
+
+
+	return 0;
+}
